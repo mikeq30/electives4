374a26c109f2c96591320580d2b3d3636533376c
Mike.R <relacionmike@gmail.com>
2024-01-17 19:30:20 +0800

    Kinopya ko yung code namen dati hehe

--- group5.py
+++ group5.py
@@ -1,5 +1,29 @@
-Hello World
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mlxtend.frequent_patterns import apriori,association_rules
+from mlxtend.preprocessing import TransactionEncoder
 
+Data = pd.read_csv('', header= None)
 
+print(Data)
+Data.replace(np.nan, 0, inplace= False)
+# change 0 value into Nan
 
+# Support percentage
+frequent_itemsets= apriori(Data, min_support= 0.10, use_colnames= True)
+print(frequent_itemsets)
+
+# Confidence Threshold
+rules= association_rules(frequent_itemsets, metric='lift', min_threshold=0.50)
+print("\n\n", rules)
+
+## A leverage value of 0 indicates independence. Range will be [-1 1]
+## A high conviction value means that the consequent is highly depending on the antecedent and range [0 inf]
+
+rules.sort_values('lift',ascending=False) # Sort Data
+print("\n\n", rules)
+print(type(rules))
+
+rules.to_csv("Apriori_Output.csv", index=False) # Get Output into CSV File
