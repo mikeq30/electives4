dd6e0f8439cf535a1d4feadc12fd9f51336966ce
Mike.R <relacionmike@gmail.com>
2024-01-17 19:42:24 +0800

    Added the csv

--- group5.py
+++ group5.py
@@ -5,22 +5,22 @@ import matplotlib.pyplot as plt
 from mlxtend.frequent_patterns import apriori,association_rules
 from mlxtend.preprocessing import TransactionEncoder
 
-Data = pd.read_csv('', header= None)
+Data = pd.read_csv('restaurant_sales.csv', header= None)
 
 print(Data)
 Data.replace(np.nan, 0, inplace= False)
 # change 0 value into Nan
 
-# Support percentage
-frequent_itemsets= apriori(Data, min_support= 0.10, use_colnames= True)
+#! Support percentage
+frequent_itemsets= apriori(Data, min_support= 0.20, use_colnames= True)
 print(frequent_itemsets)
 
-# Confidence Threshold
+#! Confidence Threshold
 rules= association_rules(frequent_itemsets, metric='lift', min_threshold=0.50)
 print("\n\n", rules)
 
-## A leverage value of 0 indicates independence. Range will be [-1 1]
-## A high conviction value means that the consequent is highly depending on the antecedent and range [0 inf]
+##* A leverage value of 0 indicates independence. Range will be [-1 1]
+##* A high conviction value means that the consequent is highly depending on the antecedent and range [0 inf]
 
 rules.sort_values('lift',ascending=False) # Sort Data
 print("\n\n", rules)
